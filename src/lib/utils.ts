d77b9c7d294492888bd3c411566f6aadea8a3194
Ahmet Palavan <ahmetpalavann@gmail.com>
2024-09-29 22:11:41 +0300

    feat: Added new utility functions for handling CSS classes and constructing metadata in the project.

---
+++ src/lib/utils.ts
@@ -0,0 +1,35 @@
+import { clsx, type ClassValue } from 'clsx';
+import { Metadata } from 'next';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function constructMetadata({
+  title = 'Memofin',
+  description = 'A simple memo app',
+  icons = '/favicon.ico',
+  image = '/hedgehog.png',
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      images: [image],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      creator: '@ahmetpalavan',
+    },
+    icons,
+    metadataBase: new URL('https://coverit-up.vercel.app/'),
+  };
+}
