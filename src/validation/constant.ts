f61eacb275733e98d5eaed4f5345509e86c16689
Ahmet Palavan <ahmetpalavann@gmail.com>
2024-10-23 17:48:47 +0300

    feat: Added validation constants for a new 'poll' feature.

--- src/validation/constant.ts
+++ src/validation/constant.ts
@@ -17,3 +17,16 @@ export const question = {
   minLength: 1,
   maxLength: 2500,
 } as const;
+
+export const poll = {
+  body: {
+    minLength: 1,
+    maxLength: 255,
+  },
+  options: {
+    minCount: 2,
+    maxCount: 6,
+    minLength: 1,
+    maxLength: 255,
+  },
+} as const;
