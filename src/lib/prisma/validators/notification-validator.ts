7032efeb86cb6f1812bc557ea8d3387446d5d0bf
Ahmet Palavan <ahmetpalavann@gmail.com>
2024-10-29 22:48:53 +0300

    feat: Add notification validator for Prisma NotificationDefaultArgs

---
+++ src/lib/prisma/validators/notification-validator.ts
@@ -0,0 +1,14 @@
+import { Prisma } from '@prisma/client';
+
+export const notificationDetail = Prisma.validator<Prisma.NotificationDefaultArgs>()({
+  include: {
+    event: {
+      select: {
+        slug: true,
+        ownerId: true,
+      },
+    },
+  },
+});
+
+export type NotificationDetail = Prisma.NotificationGetPayload<typeof notificationDetail>;
