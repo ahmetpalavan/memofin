5abd1de0ec0767038fe7e9726e75651668514335
Ahmet Palavan <ahmetpalavann@gmail.com>
2024-10-29 15:07:02 +0300

    feat: Added infinite scroll functionality to display closed polls and events in respective lists. Updated components to fetch more items as the user scrolls, improving user experience.

--- src/components/closed-polls-list.tsx
+++ src/components/closed-polls-list.tsx
@@ -2,11 +2,15 @@
 
 import { Event, Poll, User } from '@prisma/client';
 import { CircleCheckBig } from 'lucide-react';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { PollDetails } from '~/lib/prisma/validators/poll-validator';
 import { cn, PropsWithClassName } from '~/lib/utils';
 import { NoContent } from './illustrations';
 import { ClosedPoll } from './live-poll';
+import { useAction } from 'next-safe-action/hooks';
+import { getEventClosedPollsAction } from '~/lib/actions/get-event-closed-polls.action';
+import { useSearchParams } from 'next/navigation';
+import { InfiniteScrollList } from './infinite-scroll-list';
 
 type Props = PropsWithClassName<{
   initialPolls: PollDetails[];
@@ -17,6 +21,29 @@ type Props = PropsWithClassName<{
 
 export const ClosedPollsList = ({ initialPolls, ownerId, eventSlug, pollId, className }: Props) => {
   const [closedPolls, setClosedPolls] = useState(initialPolls);
+
+  const { executeAsync } = useAction(getEventClosedPollsAction);
+
+  const searchParams = useSearchParams();
+
+  const fetchMoreClosedPolls = useCallback(
+    async ({ cursor }: { cursor?: Poll['id'] }) => {
+      const newQuestion = await executeAsync({
+        eventSlug,
+        ownerId,
+        cursor,
+        pollId,
+      });
+
+      if (!newQuestion?.data || newQuestion.data.length === 0) {
+        return [];
+      }
+
+      return newQuestion.data;
+    },
+    [eventSlug, ownerId, pollId, executeAsync]
+  );
+
   return (
     <div className={cn('grid grid-cols-1 gap-4', className)}>
       {closedPolls.length === 0 ? (
@@ -26,7 +53,13 @@ export const ClosedPollsList = ({ initialPolls, ownerId, eventSlug, pollId, clas
           <p>There are no closed polls in this event.</p>
         </NoContent>
       ) : (
-        closedPolls.map((poll) => <ClosedPoll key={poll.id} poll={poll} />)
+        <InfiniteScrollList<PollDetails>
+          key={`closed-polls-${searchParams.toString()}`}
+          fetchMore={fetchMoreClosedPolls}
+          items={closedPolls}
+          setItems={setClosedPolls}
+          renderItem={(poll) => <ClosedPoll key={poll.id} poll={poll} />}
+        />
       )}
     </div>
   );
--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -1,14 +1,64 @@
+'use client';
+
+import { Event } from '@prisma/client';
+import { useAction } from 'next-safe-action/hooks';
+import { useCallback, useState } from 'react';
+import { getBookmarkedEvent } from '~/lib/actions/get-user-bookmarked-event.action';
+import { getUserEvent } from '~/lib/actions/get-user-event.action';
 import { EventDetail } from '~/lib/prisma/validators/event-validator';
 import { EventCard } from './event-card';
+import { InfiniteScrollList } from './infinite-scroll-list';
 
 interface EventsListProps {
   initialEvents: EventDetail[];
 }
 
 export const EventsList = ({ initialEvents }: EventsListProps) => {
-  return initialEvents.map((event) => <EventCard className='h-36' key={event.id} event={event} />);
+  const [events, setEvents] = useState(initialEvents);
+
+  const { executeAsync } = useAction(getUserEvent);
+
+  const fetchMoreEvents = useCallback(async ({ cursor }: { cursor?: Event['id'] }) => {
+    const newEvents = await executeAsync({ cursor });
+
+    if (!newEvents?.data || newEvents.data.length === 0) {
+      return [];
+    }
+
+    return newEvents.data;
+  }, []);
+
+  return (
+    <InfiniteScrollList<EventDetail>
+      items={events}
+      setItems={setEvents}
+      fetchMore={fetchMoreEvents}
+      renderItem={(event) => <EventCard key={event.id} event={event} />}
+    />
+  );
 };
 
 export const BookmarkedEventsList = ({ initialEvents }: EventsListProps) => {
-  return initialEvents.map((event) => <EventCard className='h-36' key={event.id} event={event} />);
+  const [events, setEvents] = useState(initialEvents);
+
+  const { executeAsync } = useAction(getBookmarkedEvent);
+
+  const fetchMore = useCallback(async ({ cursor }: { cursor?: Event['id'] }) => {
+    const newEvents = await executeAsync({ cursor });
+
+    if (!newEvents?.data || newEvents.data.length === 0) {
+      return [];
+    }
+
+    return newEvents.data;
+  }, []);
+
+  return (
+    <InfiniteScrollList<EventDetail>
+      items={events}
+      setItems={setEvents}
+      fetchMore={fetchMore}
+      renderItem={(event) => <EventCard key={event.id} event={event} />}
+    />
+  );
 };
